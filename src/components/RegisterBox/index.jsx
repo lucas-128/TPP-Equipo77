a55e29d757f0efe973087429bc0bd69859c155d4
agusbocaccio <agusbocaccio@fi.uba.ar>
2024-06-19 19:56:53 -0300

    react flow start

--- src/components/RegisterBox/index.jsx
+++ src/components/RegisterBox/index.jsx
@@ -1,8 +1,22 @@
-import React from "react";
-import { Box } from "./styled.jsx";
-import { Bus } from "../Bus/index.jsx";
-// import ArrowIcon from "./arrow.jsx";
+// import React from "react";
+// import { Box } from "./styled.jsx";
 
-export const RegisterBox = () => {
-  return <Box>Registros</Box>;
-};
+// export const RegisterBox = () => {
+//   return <Box>Registros</Box>;
+// };
+
+
+import { useState } from 'react';
+
+export const RegisterBox = (props) => {
+  const [count, setCount] = useState(props.data?.initialCount ?? 0);
+ 
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button className="nodrag" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
--- src/containers/SimulatorSection/index.jsx
+++ src/containers/SimulatorSection/index.jsx
@@ -1,16 +1,29 @@
-// import React from "react";
+import React from "react";
+import ReactFlow from "reactflow";
 
 import { Bus } from "../../components/Bus";
 import { MainMemory } from "../../components/MainMemory";
 import { RegisterBox } from "../../components/RegisterBox";
 import { Container } from "./styled";
+import "reactflow/dist/style.css";
 
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "Registros" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "Memoria principal" } },
+];
+
+const nodeTypes = {
+  register: RegisterBox,
+};
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 export const SimulatorContainer = () => {
   return (
     <Container>
-      <RegisterBox />
+      <ReactFlow nodes={nodeTypes} edges={initialEdges} />
+      {/* <RegisterBox /> */}
       {/* <Bus /> */}
-      <MainMemory />
+      {/* <MainMemory /> */}
     </Container>
   );
 };
