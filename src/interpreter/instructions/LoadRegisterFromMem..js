c5ba630261407e49684f791007fa98834e5b38ec
nicopinto22 <npinto@fi.uba.ar>
2024-08-12 01:29:42 -0300

    Fix cache memory bug when loading new values

--- src/interpreter/instructions/LoadRegisterFromMem..js
+++ src/interpreter/instructions/LoadRegisterFromMem..js
@@ -1,4 +1,3 @@
-import { typeSimulations } from "../../interpreter/constants";
 import Instruction from "../Instruction";
 import { updateCache } from "../utils";
 
@@ -8,7 +7,6 @@ Instruction: 1
 Load the content of the memory cell with address XY into register R
 
 */
-
 
 export default class LoadRegisterFromMem extends Instruction {
   constructor(register, memoryAddress, id) {
@@ -20,24 +18,17 @@ export default class LoadRegisterFromMem extends Instruction {
   execute(oldState) {
     const newExecuteState = { ...oldState.execute };
     newExecuteState.registers = [...oldState.execute.registers];
-    const { cacheMemoryCells } = oldState.execute;
     const { mainMemoryCells } = oldState.execute;
+    newExecuteState.cacheMemoryCells = updateCache(
+      newExecuteState,
+      this.memoryAddress
+    );
 
-    const cell = cacheMemoryCells.find((cell) => cell ? cell.address === this.memoryAddress : false);
-    if(cell){
-      newExecuteState.registers[this.register] = cell.content;
-      return {...oldState, execute: newExecuteState};
-    }
-    else{
-      newExecuteState.cacheMemoryCells = [...oldState.execute.cacheMemoryCells];
-      newExecuteState.cacheMemoryCells = updateCache(newExecuteState, this.memoryAddress);
-    }
-    
     const value = mainMemoryCells[this.memoryAddress];
 
     newExecuteState.registers[this.register] = value;
     newExecuteState.instructionId = this.id + 1;
 
-    return {...oldState, execute: newExecuteState};
+    return { ...oldState, execute: newExecuteState };
   }
 }
