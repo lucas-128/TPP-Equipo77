23f7fe62febbd45725092e924fe0ab2ac16c5569
Fer <fernucito@gmail.com>
2024-09-16 01:42:52 -0300

    agrego referencia de color de cada ciclo para pipelining en header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,25 +3,49 @@ import {
   HeaderTitle,
   HeaderSelect,
   HeaderOption,
+  HeaderPipeliningColorReference,
+  Cycle,
+  FetchCycle,
 } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
 import { updateTypeSimulation } from "../../slices/applicationSlice";
 import { typeSimulations } from "../../interpreter/constants";
+import { useState } from "react";
+import { FaCircle } from "react-icons/fa6";
 
 export const Header = () => {
   const dispatch = useDispatch();
-
-  const handleSelectChange = (e) => {
-    dispatch(updateTypeSimulation(e.target.value));
-  };
   const typeSimulation = useSelector(
     (state) => state.application.typeSimulation
   );
   const isSimulating = useSelector((state) => state.application.isSimulating);
+  const fetchColor = useSelector((state) => state.application.fetch.color);
+  const decodeColor = useSelector((state) => state.application.decode.color);
+  const executeColor = useSelector((state) => state.application.execute.color);
+  const [isPipelining, setIsPipelining] = useState(false);
+
+  const handleSelectChange = (e) => {
+    const selected = e.target.value;
+    // no anda el dispatch
+    selected == typeSimulations.PIPELINING
+      ? setIsPipelining(true)
+      : setIsPipelining(false);
+    dispatch(updateTypeSimulation(selected));
+  };
+
   return (
     <HeaderContainer>
       <HeaderTitle>Intérprete Máquina Ideal RISC</HeaderTitle>
-
+      {isSimulating && isPipelining && (
+        <HeaderPipeliningColorReference>
+          <FetchCycle>Fetch:</FetchCycle>
+          <FaCircle color={fetchColor} />
+          <Cycle>Decode:</Cycle>
+          <FaCircle color={decodeColor} />
+          <Cycle>Execute:</Cycle>
+          <FaCircle color={executeColor} />
+        </HeaderPipeliningColorReference>
+      )}
       <HeaderSelect
         value={typeSimulation}
         disabled={isSimulating}
